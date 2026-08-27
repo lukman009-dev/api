3683079f4b08638eccd3a0a78f3681bcaa545dad
lukman009-dev <lukmankhamismussa@gmail.com>
2026-08-27 10:01:25 +0300

    Add LoginRequest DTO for authentication

---
+++ src/main/java/com/shehia_management/api/dto/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.shehia_management.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+    private String zanId;
+    private String password;
+}
